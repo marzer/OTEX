f3569ac65140851fbb12d01c37a2bed77eef292f
Mark Gillard <marzer_@hotmail.com>
2016-11-12 23:06:56 +1030

    added PortRange

---
+++ OTEX/PortRange.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTEX
+{
+    /// <summary>
+    /// A simple class for representing a range of ports.
+    /// </summary>
+    [Serializable]
+    public sealed class PortRange
+    {
+        /////////////////////////////////////////////////////////////////////
+        // PROPERTIES/VARIABLES
+        /////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// The first port in the port range.
+        /// </summary>
+        public ushort First
+        {
+            get { return first; }
+        }
+        private ushort first;
+
+        /// <summary>
+        /// The last port in the port range.
+        /// </summary>
+        public ushort Last
+        {
+            get { return last; }
+        }
+        private ushort last;
+
+        /// <summary>
+        /// The total number of ports in the port range.
+        /// </summary>
+        public ushort Count
+        {
+            get { return (ushort)(((int)last - first) + 1); }
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // CONSTRUCTOR
+        /////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Creates a port range.
+        /// </summary>
+        /// <param name="first">The first port in the range.</param>
+        /// <param name="last">The last port in the range.</param>
+        /// <exception cref="ArgumentOutOfRangeException" />
+        public PortRange(ushort first, ushort last)
+        {
+            if (last < first)
+                throw new ArgumentOutOfRangeException("last", "last port must be equal to or greater than first port");
+            this.first = first;
+            this.last = last;
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // HELPER METHODS
+        /////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Check if a port number is contained within this range.
+        /// </summary>
+        public bool Contains(ushort port)
+        {
+            return port >= first && port <= last;
+        }
+
+        /// <summary>
+        /// Check if a port number is contained within this range.
+        /// </summary>
+        public bool Contains(uint port)
+        {
+            return port >= first && port <= last;
+        }
+
+        /// <summary>
+        /// Check if a port number is contained within this range.
+        /// </summary>
+        public bool Contains(int port)
+        {
+            return port >= first && port <= last;
+        }
+
+        /// <summary>
+        /// Check if a port number is contained within this range.
+        /// </summary>
+        public bool Contains(long port)
+        {
+            return port >= first && port <= last;
+        }
+
+        /// <summary>
+        /// Print this port range as a string.
+        /// </summary>
+        public override string ToString()
+        {
+            return string.Format("{0}-{1}",first,last);
+        }
+    }
+}
