8929f239ebd50a6a24517167ce60272e95185090
Mark Gillard <marzer_@hotmail.com>
2016-11-19 21:05:48 +1030

    added icons for Light theme mode

--- OTEXEditor/Properties/Resources.Designer.cs
+++ OTEXEditor/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace OTEX.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap cog_black {
+            get {
+                object obj = ResourceManager.GetObject("cog_black", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap logout {
             get {
                 object obj = ResourceManager.GetObject("logout", resourceCulture);
@@ -83,9 +93,9 @@ namespace OTEX.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap otex_icon_flat {
+        internal static System.Drawing.Bitmap logout_black {
             get {
-                object obj = ResourceManager.GetObject("otex_icon_flat", resourceCulture);
+                object obj = ResourceManager.GetObject("logout_black", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
