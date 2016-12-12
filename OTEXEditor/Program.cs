01efa6a5d66a66269f33476c1d09071ac875617b
Mark Gillard <marzer_@hotmail.com>
2016-12-13 06:50:54 +1030

    added x86 -> x64 bootstrap where possible

--- OTEXEditor/Program.cs
+++ OTEXEditor/Program.cs
@@ -2,8 +2,10 @@
 using Marzersoft.Themes;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace OTEX.Editor
@@ -16,6 +18,49 @@ namespace OTEX.Editor
         [STAThread]
         static void Main(string[] args)
         {
+            //run the 64-bit version if necessary
+            if (Environment.Is64BitOperatingSystem
+                && !Environment.Is64BitProcess
+                && !Debugger.IsAttached)
+            {
+                try
+                {
+#if DEBUG
+                    string dist = "Debug";
+#else
+                string dist = "Release";
+#endif
+                    string exe = Path.GetFileName(App.ExecutablePath);
+                    var x64Path = Path.Combine(App.ExecutableDirectory,
+                        Shared.InSolution ? "..\\..\\x64\\" + dist + "\\" + exe : "..\\x64\\" + exe);
+                    if (File.Exists(x64Path))
+                    {
+                        ProcessStartInfo psi = new ProcessStartInfo(x64Path);
+
+                        if (args.Length > 0)
+                        {
+                            var ar = (string[])args.Clone();
+                            for (int i = 0; i < ar.Length; ++i)
+                            {
+                                ar[i] = Regex.Replace(ar[i], @"(\\*)" + "\"", @"$1$1\" + "\"");
+                                ar[i] = "\"" + Regex.Replace(ar[i], @"(\\+)$", @"$1$1") + "\"";
+                            }
+                            psi.Arguments = string.Join(" ", ar);
+                        }
+
+                        if (Path.GetFullPath(Environment.CurrentDirectory).ToLower()
+                            .Equals(Path.GetFullPath(App.ExecutableDirectory).ToLower()))
+                            psi.WorkingDirectory = Path.GetFullPath(Path.GetDirectoryName(x64Path));
+                        else
+                            psi.WorkingDirectory = Environment.CurrentDirectory;
+
+                        Process.Start(psi);
+                        return;
+                    }
+                }
+                catch (Exception) { }
+            }
+
             App.MainFormType = typeof(EditorForm);
             App.Name = "OTEX Editor";
             App.Description = "Editor client for OTEX collaborative text editor framework.";
